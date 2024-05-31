c6f5f8a2bcfbea8b019b615c4925d3f2a2b60659
DavHau <hsngrmpf+github@gmail.com>
2024-05-31 16:13:28 +0200

    secrets: fallback to loading from env variable

--- credentials.py
+++ credentials.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 
 def get_secret(secret_name):
+    env_var_name = f"SECRET_{secret_name.upper().replace('-', '_')}"
+    if env_var_name in os.environ:
+        return os.environ[env_var_name]
     secrets_directory = os.environ.get('CREDENTIALS_DIRECTORY', '.')
     secrets_directory = Path(secrets_directory)
 
