9cb2938ef83b522b586a5952aa01a268176ead4b
shoutingcatana <hauerstefan31@gmail.com>
2024-06-19 15:51:45 +0200

    add translation function

--- summarize.py
+++ summarize.py
@@ -3,20 +3,20 @@ import openai
 import credentials
 
 
-def create_prompt(message):
+def create_prompt(message, language):
     # creating a prompt for gpt
-    prompt = f"Fasse folgende Nachricht zusammen, falls diese länger als 20 Wörter ist sonst schicke sie unbearbeitet " \
-             f"zuruück: {message}." \
+    prompt = f"Übersetze folgende Nachricht in {language}, fasse sie zusammen falls diese länger als 20 Wörter ist, sonst schicke" \
+             f" sie unbearbeitet zurück: {message}." \
 
     return prompt
 
 
-def gpt_message_handler(message):
+def gpt_message_handler(message, language):
     # Read the API key from a file
     api_key = credentials.get_secret("gpt-api-key")
 
     # Create a prompt from the input message
-    prompt = create_prompt(message)
+    prompt = create_prompt(message, language)
 
     # Set the OpenAI API key
     openai.api_key = api_key
--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,8 @@ import telebot
 import voice_conveter
 import summarize
 import credentials
+from telebot import TeleBot
+from telebot import types
 # from pydub.utils import mediainfo
 
 
@@ -14,6 +16,26 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['start', 'suck'])
 def send_welcome(message):
     bot.reply_to(message, "i love you too?")
+
+
+@bot.message_handler(commands=['configure'])
+def configure(message):
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    english = types.InlineKeyboardButton("english", callback_data="english")
+    spanish = types.InlineKeyboardButton("spanish", callback_data="spanish")
+    german = types.InlineKeyboardButton("german", callback_data="german")
+
+    markup.add(english, spanish, german)
+
+    bot.send_message(message.chat.id, 'Into which languages should the message be translated?', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def answer(callback):
+    global language
+    if callback.message:
+        bot.send_message(callback.message.chat.id, f"All your messages will now be translated to {callback.data}!")
+        language = callback.data
 
 
 @bot.message_handler(func=lambda msg: True)
@@ -41,15 +63,16 @@ def voice_processing(message):
         answer_0 = bot.reply_to(message, output_text)
         chat_id = answer_0.chat.id
         message_id = answer_0.message_id
-        output_text = summarize.gpt_message_handler(output_text)
-    answer_1 = bot.edit_message_text(output_text, chat_id, message_id)
+
+        output_text = summarize.gpt_message_handler(output_text, language)
+        bot.edit_message_text(output_text, chat_id, message_id)
 
     # run the bigger neuronal network and edit message one
     """MESSAGE 2"""
     # define message- and chat-id from the first message
     used_model = voice_conveter.model_2
     answer_2 = voice_conveter.transcribe(str(voice_file.name), used_model)
-    answer_2 = summarize.gpt_message_handler(answer_2)
+    answer_2 = summarize.gpt_message_handler(answer_2, language)
     bot.edit_message_text(answer_2, chat_id, message_id)
 
 
